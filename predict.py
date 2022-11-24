326c5a84c690f98b6d7f24573d1e1f2904d6bd0c
cabbagec2hlbGwK <cabbageshell@protonmail.com>
2022-11-24 17:35:32 -0500

    changed the structure and file chanes\n predict gusser

--- predict.py
+++ predict.py
@@ -1,5 +1,7 @@
 import pickle
 import pandas as pd
+
+
 def pred(classifier, csvv, pre):
     modelPath = f'model/{classifier}'
     try:
@@ -8,14 +10,20 @@ def pred(classifier, csvv, pre):
     except:
         pass
     d = pd.read_csv(csvv)
-    d['r1'] = d['r1'].shift(-1*pre)
+    # d = d.drop(d[d['time '] == 2].index)
+    # d.reset_index()
+    # d = d.drop(columns="time ")
+    d = d.sort_values(by=["year", "Mon", "date"])
+    d.reset_index()
     print(d)
+    return
+    d['r1'] = d['r1'].shift(-1*pre)
     print("data")
     print(model.predict(d[:-1*pre]))
 
 
 def main():
-    pred("long_run_1day.pickle","data/data.csv", 5)
+    pred("long_run_1day.pickle", "data/predict/data.csv", 1)
 
 
 if __name__ == "__main__":
